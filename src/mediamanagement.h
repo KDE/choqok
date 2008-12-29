9b9dc43d396ef3f9bcaf37647f13301f4cbe6d9b
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2008-12-29 09:12:26 +0000

    Copyright header added to source files
    svn path=/trunk/playground/network/choqok/; revision=902805

--- src/mediamanagement.h
+++ src/mediamanagement.h
@@ -1,14 +1,23 @@
-//
-// C++ Interface: mediamanagement
-//
-// Description: 
-//
-//
-// Author:  Mehrdad Momeny <mehrdad.momeny@gmail.com>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/*
+    This file is part of choqoK, the KDE Twitter client
+
+    Copyright (C) 2008 Mehrdad Momeny <mehrdad.momeny@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+*/
 #ifndef MEDIAMANAGEMENT_H
 #define MEDIAMANAGEMENT_H
 
