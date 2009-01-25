6af813338bb6dd4e7abc22e2caec710c64760caf
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2009-01-25 19:29:22 +0000

    License headers are updated, version number updated to 0.4 and about description updated.
    svn path=/trunk/playground/network/choqok/; revision=916693

--- src/account.cpp
+++ src/account.cpp
@@ -1,12 +1,16 @@
 /*
     This file is part of choqoK, the KDE Twitter client
 
-    Copyright (C) 2008 Mehrdad Momeny <mehrdad.momeny@gmail.com>
+    Copyright (C) 2008-2009 Mehrdad Momeny <mehrdad.momeny@gmail.com>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy 
+    defined in Section 14 of version 3 of the license.
+
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +18,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+    along with this program; if not, see http://www.gnu.org/licenses/
 
 */
 #include "account.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 /*
     This file is part of choqoK, the KDE Twitter client
 
-    Copyright (C) 2008 Mehrdad Momeny <mehrdad.momeny@gmail.com>
+    Copyright (C) 2008-2009 Mehrdad Momeny <mehrdad.momeny@gmail.com>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy 
+    defined in Section 14 of version 3 of the license.
+
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +18,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+    along with this program; if not, see http://www.gnu.org/licenses/
 
 */
 
@@ -27,12 +30,17 @@
 #include <KDE/KLocale>
 
 static const char description[] =
-    I18N_NOOP("A KDE 4 Twitter Client");
+    I18N_NOOP("KDE mono-blogging client,\n\
+Currently supports <a href='http://twitter.com'>Twitter.com</a> and \
+<a href='http://identi.ca'>Identi.ca</a>");
+
+static const char version[] = "0.4";
 
 int main(int argc, char **argv)
 {
-	KAboutData about("choqok", 0, ki18n("choqoK"), VERSION, ki18n(description),
-                     KAboutData::License_GPL_V3, ki18n("(C) 2008 Mehrdad Momeny"), KLocalizedString(), 0, "mehrdad.momeny@gmail.com");
+	KAboutData about("choqok", 0, ki18n("choqoK"), version, ki18n(description),
+                     KAboutData::License_GPL_V3, ki18n("(C) 2008-2009 Mehrdad Momeny"),
+                             KLocalizedString(), 0, "mehrdad.momeny@gmail.com");
     about.addAuthor( ki18n("Mehrdad Momeny"), ki18n("Author and Core Developer"),
 					 "mehrdad.momeny@gmail.com");
 	about.addCredit(ki18n("Roozbeh Shafiee"), ki18n("Icon designer"), "roozbeh@roozbehonline.com");
