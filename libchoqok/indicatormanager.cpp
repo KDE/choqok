b7e1f416d41c08678273f123a88f155549ad3845
Andrey Esin <gmlastik@gmail.com>
2010-11-07 00:06:48 +0300

    Forgot to add new files :( sorry - Indicator can show count of unreaded home timelines messages.

---
+++ libchoqok/indicatormanager.cpp
@@ -0,0 +1,91 @@
+/*
+    This file is part of Choqok, the KDE micro-blogging client
+
+    Copyright (C) 2010 Andrey Esin <gmlastik@gmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy
+    defined in Section 14 of version 3 of the license.
+
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see http://www.gnu.org/licenses/
+
+*/
+
+#include "indicatormanager.h"
+#include <QApplication>
+#include "choqokuiglobal.h"
+
+#include <QDebug>
+#include <KIcon>
+
+#define STR(x) #x
+#define XSTR(x) STR(x)
+
+namespace Choqok
+{
+
+MessageIndicatorManager::MessageIndicatorManager()
+{
+    iServer = QIndicate::Server::defaultInstance();
+    iServer->setType("message.irc");
+    QString desktopFile = QString("%1/%2.desktop")
+    .arg(XSTR(XDG_APPS_INSTALL_DIR))
+    .arg(QCoreApplication::applicationFilePath().section('/', -1));    
+
+    iServer->setDesktopFile(desktopFile);
+    iServer->show();
+    
+    iIndicator = new QIndicate::Indicator(this);
+    iIndicator->setNameProperty( "Home" );
+    iIndicator->setIconProperty(KIcon( "user-home" ).pixmap(QSize(16, 16), QIcon::Normal, QIcon::On).toImage());
+    
+    //iIndicator->show();
+    connect( iIndicator, SIGNAL(display(QIndicate::Indicator*)), SLOT(slotDisplay(QIndicate::Indicator*)));
+    allUnread = 0;
+    
+}
+
+MessageIndicatorManager::~MessageIndicatorManager()
+{
+}
+
+void MessageIndicatorManager::newPostInc( int unread, const QString& alias, const QString& timeline )
+{
+  Q_UNUSED(unread);
+  Q_UNUSED(alias);
+
+  if ( timeline == QString( "Home" )){
+    iIndicator->setDrawAttentionProperty( true );
+    iIndicator->show();
+    iIndicator->setCountProperty( allUnread += unread );
+  }
+}
+
+void MessageIndicatorManager::slotDisplay(QIndicate::Indicator* )
+{
+  iIndicator->hide();
+  iIndicator->setDrawAttentionProperty(false);
+  choqokMainWindow->activateChoqok();
+}
+
+MessageIndicatorManager * MessageIndicatorManager::mSelf = NULL;
+
+MessageIndicatorManager * MessageIndicatorManager::self()
+{
+    if ( !mSelf )
+        mSelf = new MessageIndicatorManager;
+    return mSelf;
+}
+
+}
---
+++ libchoqok/indicatormanager.h
@@ -0,0 +1,61 @@
+/*
+    This file is part of Choqok, the KDE micro-blogging client
+
+    Copyright (C) 2010 Andrey Esin <gmlastik@gmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy
+    defined in Section 14 of version 3 of the license.
+
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see http://www.gnu.org/licenses/
+
+*/
+
+#ifndef INDICATORMANAGER_H
+#define INDICATORMANAGER_H
+
+#include <KDE/KNotification>
+#include <klocalizedstring.h>
+#include "choqok_export.h"
+
+#ifdef HAVE_INDICATEQT
+ #include <qindicateserver.h>
+ #include <qindicateindicator.h>
+#endif
+
+namespace Choqok
+{
+  
+class CHOQOK_EXPORT MessageIndicatorManager : public QObject
+{
+  Q_OBJECT
+  public:
+    static MessageIndicatorManager* self();
+    ~MessageIndicatorManager();
+    void newPostInc( int unread, const QString& alias, const QString& timeline );
+
+#ifdef HAVE_INDICATEQT
+    QIndicate::Server *iServer;
+    QIndicate::Indicator *iIndicator;
+#endif
+
+  private:
+    MessageIndicatorManager();
+    static MessageIndicatorManager *mSelf;
+    int allUnread;
+public slots:
+    void slotDisplay(QIndicate::Indicator*);
+};
+}
+#endif // INDICATORMANAGER_H
