e1e3470d813139baf74e91f79a1213ebd4540e2c
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2010-12-02 00:10:09 +0330

    And final fix to compile choqok with libindicate too :D CCBUG:258329

--- cmake/modules/FindQtIndicate.cmake
+++ cmake/modules/FindQtIndicate.cmake
@@ -2,7 +2,7 @@
 # Once done this will define
 #
 #  QTINDICATE_FOUND - system has the QtIndicate library
-#  QTINDICATE_INCLUDE_DIR - the QtIndicate include directory
+#  QTINDICATE_INCLUDE_DIRS - the QtIndicate include directories
 #  QTINDICATE_LIBRARY - Link this to use the QtIndicate library
 #
 # Copyright (c) 2010, Mehrdad Momeny <mehrdad.momeny@gmail.com>
@@ -10,29 +10,28 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (QTINDICATE_INCLUDE_DIR AND QTINDICATE_LIBRARY)
+if (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
     # in cache already
 set(QTINDICATE_FOUND TRUE)
 
-else (QTINDICATE_INCLUDE_DIR AND QTINDICATE_LIBRARY)
+else (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
     if (NOT WIN32)
         find_package(PkgConfig)
-        pkg_check_modules(PC_QTINDICATE indicate-qt>=0.2.1)
+        pkg_check_modules(PC_QTINDICATE indicate-qt)
     endif(NOT WIN32)
 
-    find_path(QTINDICATE_INCLUDE_DIR libindicate-qt/qindicateserver.h
-    HINTS
-    ${PC_QTINDICATE_INCLUDE_DIRS}
+    find_library(QTINDICATE_LIBRARY NAMES indicate-qt
+    HINTS  ${PC_QTINDICATE_LIBDIR} ${PC_QTINDICATE_LIBRARY_DIRS}
     )
 
-    find_library(QTINDICATE_LIBRARY NAMES libindicate-qt
-    HINTS
-    ${PC_QTINDICATE_LIBRARY_DIRS}
+    find_path(QTINDICATE_INCLUDE_DIR libindicate-qt/qindicateserver.h
+    HINTS ${PC_QTINDICATE_INCLUDEDIR} ${PC_QTINDICATE_INCLUDE_DIRS}
     )
+    set(QTINDICATE_INCLUDE_DIRS ${QTINDICATE_INCLUDE_DIR} ${QTINDICATE_INCLUDE_DIR}/libindicate-qt)
 
     include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(QtIndicate DEFAULT_MSG QTINDICATE_LIBRARY QTINDICATE_INCLUDE_DIR)
+    find_package_handle_standard_args(QtIndicate DEFAULT_MSG QTINDICATE_LIBRARY QTINDICATE_INCLUDE_DIRS)
 
-    mark_as_advanced(QTINDICATE_INCLUDE_DIR QTINDICATE_LIBRARY)
+    mark_as_advanced(QTINDICATE_INCLUDE_DIRS QTINDICATE_LIBRARY)
 
-endif (QTINDICATE_INCLUDE_DIR AND QTINDICATE_LIBRARY)
+    endif (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
