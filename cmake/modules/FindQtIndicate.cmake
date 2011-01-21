6e9d7dc74f2caab04b1fc840427e25c4a3bed214
Theo Chatzimichos <tampakrap@gentoo.org>
2011-01-21 19:16:42 +0200

    Finishing commit 81969e4, actuall fix the automagic dep to libindicate-qt, bug was in FindQtIndicate.cmake Rename two cmake variables:  - QTINDICATE_FOUND -> QtIndicate_FOUND (to match the name of the Find module)  - HAVE_INDICATEQT -> QTINDICATE_BUILD (makes more sense now)

--- cmake/modules/FindQtIndicate.cmake
+++ cmake/modules/FindQtIndicate.cmake
@@ -1,7 +1,7 @@
 # - Try to find the indicate-qt library
 # Once done this will define
 #
-#  QTINDICATE_FOUND - system has the QtIndicate library
+#  QtIndicate_FOUND - system has the QtIndicate library
 #  QTINDICATE_INCLUDE_DIRS - the QtIndicate include directories
 #  QTINDICATE_LIBRARY - Link this to use the QtIndicate library
 #
@@ -10,28 +10,25 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
-    # in cache already
-set(QTINDICATE_FOUND TRUE)
+if (NOT WIN32)
+    find_package(PkgConfig)
+    pkg_check_modules(PC_QTINDICATE indicate-qt)
+endif(NOT WIN32)
 
-else (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
-    if (NOT WIN32)
-        find_package(PkgConfig)
-        pkg_check_modules(PC_QTINDICATE indicate-qt)
-    endif(NOT WIN32)
-
-    find_library(QTINDICATE_LIBRARY NAMES indicate-qt
+find_library(QTINDICATE_LIBRARY NAMES indicate-qt
     HINTS  ${PC_QTINDICATE_LIBDIR} ${PC_QTINDICATE_LIBRARY_DIRS}
-    )
+)
 
-    find_path(QTINDICATE_INCLUDE_DIR libindicate-qt/qindicateserver.h
+find_path(QTINDICATE_INCLUDE_DIR libindicate-qt/qindicateserver.h
     HINTS ${PC_QTINDICATE_INCLUDEDIR} ${PC_QTINDICATE_INCLUDE_DIRS}
-    )
-    set(QTINDICATE_INCLUDE_DIRS ${QTINDICATE_INCLUDE_DIR} ${QTINDICATE_INCLUDE_DIR}/libindicate-qt)
+)
+set(QTINDICATE_INCLUDE_DIRS ${QTINDICATE_INCLUDE_DIR} ${QTINDICATE_INCLUDE_DIR}/libindicate-qt)
 
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(QtIndicate DEFAULT_MSG QTINDICATE_LIBRARY QTINDICATE_INCLUDE_DIRS)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(QtIndicate DEFAULT_MSG QTINDICATE_LIBRARY QTINDICATE_INCLUDE_DIRS)
 
-    mark_as_advanced(QTINDICATE_INCLUDE_DIRS QTINDICATE_LIBRARY)
+mark_as_advanced(QTINDICATE_INCLUDE_DIRS QTINDICATE_LIBRARY)
 
-    endif (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
+if (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
+    set(QtIndicate_FOUND TRUE)
+endif (QTINDICATE_INCLUDE_DIRS AND QTINDICATE_LIBRARY)
