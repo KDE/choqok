e884a0b7d96b0554b32967edcd3271eb646c4b9e
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2011-01-27 20:45:47 +0330

    master will be next stable release, and versions updated to 1.0.1

--- choqok/main.cpp
+++ choqok/main.cpp
@@ -32,7 +32,7 @@
 static const char description[] =
     I18N_NOOP( "KDE Micro-Blogging Client." );
 
-static const char version[] = "0.9.99";
+static const char version[] = "1.0.1";
 
 int main( int argc, char **argv )
 {
