fd2f890faf1ec526fab151dadb650fd0afaf4471
Farhad Hedayati Fard <hf.farhad@gmail.com>
2011-03-16 23:04:05 +0330

    fix coding style and fixed a bug in quickfilter plugin with multiple open microblogs, plus some minor bug fixes!

--- plugins/quickfilter/quickfilter.cpp
+++ plugins/quickfilter/quickfilter.cpp
@@ -1,19 +1,19 @@
 /*
-    Copyright (C) 2011  Farhad Hedayati-Fard <hf.farhad@gmail.com>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *    Copyright (C) 2011  Farhad Hedayati-Fard <hf.farhad@gmail.com>
+ * 
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 #include "quickfilter.h"
@@ -40,124 +40,125 @@ K_EXPORT_PLUGIN( MyPluginFactory( "choqok_quickfilter" ) )
 
 QuickFilter::QuickFilter(QObject* parent, const QList< QVariant >& args) : Choqok::Plugin(MyPluginFactory::componentData(), parent)
 {
-  m_authorAction = new KAction(KIcon("document-preview"), i18n("Filter by author"), this);
-  m_authorAction->setCheckable(true);
-  m_textAction = new KAction(KIcon("document-preview"), i18n("Filter by content"), this);
-  m_textAction->setCheckable(true);
-  actionCollection()->addAction("filterByAuthor", m_authorAction);
-  actionCollection()->addAction("filterByContent", m_textAction);
-  connect(m_authorAction, SIGNAL(toggled(bool)), SLOT(showAuthorFilterUiInterface(bool)));
-  connect(m_textAction, SIGNAL(toggled(bool)), SLOT(showTextFilterUiInterface(bool)));
-  setXMLFile("quickfilterui.rc");
-  createUiInterface();
+    m_authorAction = new KAction(KIcon("document-preview"), i18n("Filter by author"), this);
+    m_authorAction->setCheckable(true);
+    m_textAction = new KAction(KIcon("document-preview"), i18n("Filter by content"), this);
+    m_textAction->setCheckable(true);
+    actionCollection()->addAction("filterByAuthor", m_authorAction);
+    actionCollection()->addAction("filterByContent", m_textAction);
+    connect(m_authorAction, SIGNAL(toggled(bool)), SLOT(showAuthorFilterUiInterface(bool)));
+    connect(m_textAction, SIGNAL(toggled(bool)), SLOT(showContentFilterUiInterface(bool)));
+    setXMLFile("quickfilterui.rc");
+    createUiInterface();
+    connect(Choqok::UI::Global::mainWindow(), SIGNAL(currentMicroBlogWidgetChanged(Choqok::UI::MicroBlogWidget*)), this, SLOT(showAllPosts()));
 }
 
 QuickFilter::~QuickFilter()
 {
-
+    
 }
 
 void QuickFilter::filterByAuthor()
 {
-  m_filterUser = m_aledit->text();
-  if (!m_filterUser.isEmpty()) {
-    foreach(Choqok::UI::PostWidget* postwidget, Choqok::UI::Global::mainWindow()->currentMicroBlog()->currentTimeline()->postWidgets()) {
-	if (postwidget->currentPost().author.userName != m_filterUser) {
-	  postwidget->hide();
-      }
+    m_filterUser = m_aledit->text();
+    if (!m_filterUser.isEmpty()) {
+        foreach(Choqok::UI::PostWidget* postwidget, Choqok::UI::Global::mainWindow()->currentMicroBlog()->currentTimeline()->postWidgets()) {
+            if (postwidget->currentPost().author.userName != m_filterUser) {
+                postwidget->hide();
+            }
+        }
     }
-  }
-  else {
-    foreach(Choqok::UI::PostWidget* postwidget, Choqok::UI::Global::mainWindow()->currentMicroBlog()->currentTimeline()->postWidgets()) {
-	postwidget->show();
+    else {
+        showAllPosts();
     }
-  }
 }
 
-void QuickFilter::filterByText()
+void QuickFilter::filterByContent()
 {
-  m_filterText = m_tledit->text();
-  if (!m_filterText.isEmpty()) {
-    foreach(Choqok::UI::PostWidget* postwidget, Choqok::UI::Global::mainWindow()->currentMicroBlog()->currentTimeline()->postWidgets()) {
-	if ( ! postwidget->currentPost().content.contains(m_filterText, Qt::CaseInsensitive) ) {
-	  postwidget->hide();
-	}
-      }
-  }
-  else {
-    foreach(Choqok::UI::PostWidget* postwidget, Choqok::UI::Global::mainWindow()->currentMicroBlog()->currentTimeline()->postWidgets()) {
-	postwidget->show();
+    m_filterText = m_tledit->text();
+    if (!m_filterText.isEmpty()) {
+        foreach(Choqok::UI::PostWidget* postwidget, Choqok::UI::Global::mainWindow()->currentMicroBlog()->currentTimeline()->postWidgets()) {
+            if ( ! postwidget->currentPost().content.contains(m_filterText, Qt::CaseInsensitive) ) {
+                postwidget->hide();
+            }
+        }
     }
-  }
+    else {
+        showAllPosts();
+    }
 }
 
 void QuickFilter::createUiInterface()
 {
-  m_authorToolbar = new QToolBar(Choqok::UI::Global::mainWindow());
-  m_textToolbar = new QToolBar(Choqok::UI::Global::mainWindow());
-  m_aledit = new KLineEdit(m_authorToolbar);
-  m_aledit->setClearButtonShown(true);
-
-  m_tledit = new KLineEdit(m_textToolbar);
-  m_tledit->setClearButtonShown(true);
-
-  
-  QLabel *alabel = new QLabel(i18n("Author"), m_authorToolbar);
-  QLabel *tlabel = new QLabel(i18n("Text"), m_textToolbar);
-  m_authorToolbar->addWidget(alabel);
-  m_authorToolbar->addWidget(m_aledit);
-  QPushButton *authorCloseButton = new QPushButton(KIcon("dialog-close"), QString() , m_authorToolbar);
-  authorCloseButton->setMaximumWidth(authorCloseButton->height());
-  connect(authorCloseButton, SIGNAL(clicked(bool)), this, SLOT(hideAuthorFilterbar()));
-  m_authorToolbar->addWidget(authorCloseButton);
-  
-  m_textToolbar->addWidget(tlabel);
-  m_textToolbar->addWidget(m_tledit);
-  QPushButton *textCloseButton = new QPushButton(KIcon("dialog-close"), QString() , m_textToolbar);
-  textCloseButton->setMaximumWidth(textCloseButton->height());
-  connect(textCloseButton, SIGNAL(clicked(bool)), this, SLOT(hideTextFilterbar()));
-  m_textToolbar->addWidget(textCloseButton);
-
-  connect(m_aledit, SIGNAL(editingFinished()), this , SLOT(filterByAuthor()));
-  connect(m_aledit, SIGNAL(textChanged(QString)), this, SLOT(updateUser(QString)));
-  
-  connect(m_tledit, SIGNAL(editingFinished()), this, SLOT(filterByText()));
-  connect(m_tledit, SIGNAL(textChanged(QString)), this, SLOT(updateText(QString)));
-  
-  Choqok::UI::Global::mainWindow()->addToolBar(Qt::BottomToolBarArea, m_authorToolbar);
-  Choqok::UI::Global::mainWindow()->addToolBar(Qt::BottomToolBarArea, m_textToolbar);
-  m_authorToolbar->hide();
-  m_textToolbar->hide();
+    m_authorToolbar = new QToolBar(Choqok::UI::Global::mainWindow());
+    m_textToolbar = new QToolBar(Choqok::UI::Global::mainWindow());
+    m_aledit = new KLineEdit(m_authorToolbar);
+    m_aledit->setClearButtonShown(true);
+    
+    m_tledit = new KLineEdit(m_textToolbar);
+    m_tledit->setClearButtonShown(true);
+    
+    
+    QLabel *alabel = new QLabel(i18n("Author"), m_authorToolbar);
+    QLabel *tlabel = new QLabel(i18n("Text"), m_textToolbar);
+    m_authorToolbar->addWidget(alabel);
+    m_authorToolbar->addWidget(m_aledit);
+    QPushButton *authorCloseButton = new QPushButton(KIcon("dialog-close"), QString() , m_authorToolbar);
+    authorCloseButton->setMaximumWidth(authorCloseButton->height());
+    connect(authorCloseButton, SIGNAL(clicked(bool)), this, SLOT(hideAuthorFilterbar()));
+    m_authorToolbar->addWidget(authorCloseButton);
+    
+    m_textToolbar->addWidget(tlabel);
+    m_textToolbar->addWidget(m_tledit);
+    QPushButton *textCloseButton = new QPushButton(KIcon("dialog-close"), QString() , m_textToolbar);
+    textCloseButton->setMaximumWidth(textCloseButton->height());
+    connect(textCloseButton, SIGNAL(clicked(bool)), this, SLOT(hideContentFilterbar()));
+    m_textToolbar->addWidget(textCloseButton);
+    
+    connect(m_aledit, SIGNAL(editingFinished()), this , SLOT(filterByAuthor()));
+    connect(m_aledit, SIGNAL(textChanged(QString)), this, SLOT(updateUser(QString)));
+    
+    connect(m_tledit, SIGNAL(editingFinished()), this, SLOT(filterByContent()));
+    connect(m_tledit, SIGNAL(textChanged(QString)), this, SLOT(updateContent(QString)));
+    
+    Choqok::UI::Global::mainWindow()->addToolBar(Qt::BottomToolBarArea, m_authorToolbar);
+    Choqok::UI::Global::mainWindow()->addToolBar(Qt::BottomToolBarArea, m_textToolbar);
+    m_authorToolbar->hide();
+    m_textToolbar->hide();
 }
 
 void QuickFilter::showAuthorFilterUiInterface(bool show)
 {
-    if (show) 
+    if (show) {
         m_authorToolbar->show();
+        m_aledit->setFocus();
+    }
     else
         m_authorToolbar->hide();
 }
 
-void QuickFilter::showTextFilterUiInterface(bool show)
+void QuickFilter::showContentFilterUiInterface(bool show)
 {
-    if (show)
+    if (show) {
         m_textToolbar->show();
+        m_tledit->setFocus();
+    }
     else
         m_textToolbar->hide();
 }
 
 void QuickFilter::updateUser(QString user)
 {
-  if (user.isEmpty()) {
-      filterByAuthor();
-  }
+    if (user.isEmpty()) {
+        filterByAuthor();
+    }
 }
 
-void QuickFilter::updateText(QString text)
+void QuickFilter::updateContent(QString text)
 {
-  if (text.isEmpty()) {
-      filterByText();
-  }
+    if (text.isEmpty()) {
+        filterByContent();
+    }
 }
 
 void QuickFilter::hideAuthorFilterbar()
@@ -166,12 +167,18 @@ void QuickFilter::hideAuthorFilterbar()
     m_authorAction->setChecked(false);
 }
 
-void QuickFilter::hideTextFilterbar()
+void QuickFilter::hideContentFilterbar()
 {
     m_textToolbar->hide();
     m_textAction->setChecked(false);
 }
 
+void QuickFilter::showAllPosts()
+{
+    foreach(Choqok::UI::PostWidget* postwidget, Choqok::UI::Global::mainWindow()->currentMicroBlog()->currentTimeline()->postWidgets()) {
+        postwidget->show();
+    }
+}
 
 
 
--- plugins/quickfilter/quickfilter.h
+++ plugins/quickfilter/quickfilter.h
@@ -1,19 +1,19 @@
 /*
-    Copyright (C) 2011  Farhad Hedayati-Fard <hf.farhad@gmail.com>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *    Copyright (C) 2011  Farhad Hedayati-Fard <hf.farhad@gmail.com>
+ * 
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 #ifndef QUICKFILTER_H
@@ -25,10 +25,10 @@
 class KAction;
 namespace Choqok {
     class ShortenManager;
-namespace UI {
-    class PostWidget;
-    class MicroBlogWidget;
-}
+    namespace UI {
+        class PostWidget;
+        class MicroBlogWidget;
+    }
 }
 
 class KLineEdit;
@@ -36,35 +36,36 @@ class QToolBar;
 
 class QuickFilter : public Choqok::Plugin
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  QuickFilter(QObject* parent, const QList< QVariant >& args);
-  ~QuickFilter();
-
+    QuickFilter(QObject* parent, const QList< QVariant >& args);
+    ~QuickFilter();
+    
 public slots:
-  void createUiInterface();
-  void showAuthorFilterUiInterface(bool);
-  void showTextFilterUiInterface(bool);
-  void hideTextFilterbar();
-  void hideAuthorFilterbar();
-
+    void createUiInterface();
+    void showAuthorFilterUiInterface(bool);
+    void showContentFilterUiInterface(bool);
+    void hideContentFilterbar();
+    void hideAuthorFilterbar();
+    void showAllPosts();
+    
 protected slots:
-  void filterByAuthor();
-  void filterByText();
-
+    void filterByAuthor();
+    void filterByContent();
+    
 private slots:
-  void updateUser(QString user);
-  void updateText(QString text);
-
+    void updateUser(QString user);
+    void updateContent(QString text);
+    
 private:
-  QString m_filterUser;
-  QString m_filterText;
-  KLineEdit *m_aledit;
-  KLineEdit *m_tledit;
-  QToolBar *m_authorToolbar;
-  QToolBar *m_textToolbar;
-  KAction *m_authorAction;
-  KAction *m_textAction;
+    QString m_filterUser;
+    QString m_filterText;
+    KLineEdit *m_aledit;
+    KLineEdit *m_tledit;
+    QToolBar *m_authorToolbar;
+    QToolBar *m_textToolbar;
+    KAction *m_authorAction;
+    KAction *m_textAction;
 };
 
 #endif // QUICKFILTER_H
