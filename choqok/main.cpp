d48aa49b1fdf5b33fc738856e712dda820739dc8
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2011-05-09 16:22:19 +0430

    changed the version to be distinguishable.

--- choqok/main.cpp
+++ choqok/main.cpp
@@ -32,7 +32,7 @@
 static const char description[] =
     I18N_NOOP( "KDE Micro-Blogging Client." );
 
-static const char version[] = "1.1";
+static const char version[] = "1.1.5";
 
 int main( int argc, char **argv )
 {
