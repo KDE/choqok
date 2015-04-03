KDE/choqok

commit 98aecec8a067d204e85ecdd18d744a9a20c5128f

Use HTTPS when available

plugins/imagepreview/imagepreview.cpp
@@ -35,7 +35,7 @@
 K_PLUGIN_FACTORY_WITH_JSON(ImagePreviewFactory, "choqok_imagepreview.json",
                            registerPlugin < ImagePreview > ();)
 
-const QRegExp ImagePreview::mTwitpicRegExp("(http://twitpic.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");
+const QRegExp ImagePreview::mTwitpicRegExp("(https://twitpic.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");
 const QRegExp ImagePreview::mYFrogRegExp("(http://yfrog.[^\\s<>\"]+[^!,\\.\\s<>'\\\"\\]])");
 const QRegExp ImagePreview::mTweetphotoRegExp("(http://tweetphoto.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");
 const QRegExp ImagePreview::mPlixiRegExp("(http://plixi.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");
@@ -96,13 +96,13 @@ void ImagePreview::parse(Choqok::UI::PostWidget *postToParse)
     QStringList PumpIORedirectList;
     QString content = postToParse->currentPost()->content;
 
-    //Twitpic: http://www.twitpic.com/api.do
+    //Twitpic: https://www.twitpic.com/api.do
     while ((pos = mTwitpicRegExp.indexIn(content, pos)) != -1) {
         pos += mTwitpicRegExp.matchedLength();
         twitpicRedirectList << mTwitpicRegExp.cap(0);
     }
     Q_FOREACH (const QString &url, twitpicRedirectList) {
-        QString twitpicUrl = QString("http://twitpic.com/show/mini%1").arg(QString(url).remove("http://twitpic.com"));
+        QString twitpicUrl = QString("https://twitpic.com/show/mini%1").arg(QString(url).remove("https://twitpic.com"));
         connect(Choqok::MediaManager::self(),
                 SIGNAL(imageFetched(QString,QPixmap)),
                 SLOT(slotImageFetched(QString,QPixmap)));

choqok/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
     about.addAuthor(i18n("Mehrdad Momeny"), i18n("Author, Developer and Maintainer"),
                     "mehrdad.momeny@gmail.com", "http://momeny.wordpress.com");
     about.addAuthor(i18n("Andrey Esin"), i18n("Developer"),
-                    "gmlastik@gmail.com", "http://twitter.com/la_stik");
+                    "gmlastik@gmail.com", "https://twitter.com/la_stik");
     about.addAuthor(i18n("Andrea Scarpino"), i18n("Developer"),
                     "scarpino@kde.org", "http://www.andreascarpino.it");
 
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
     about.addCredit(i18n("Daniel Schaal"), i18n("UI improvements"), "daniel@foto-schaal.de");
     about.addCredit(i18n("Stephen Henderson"), i18n("Search API implementation"), "hendersonsk@gmail.com");
     about.addCredit(i18n("Tejas Dinkar"), i18n("Developer"),
-                    "tejasdinkar@gmail.com", "http://twitter.com/tdinkar");
+                    "tejasdinkar@gmail.com", "https://twitter.com/tdinkar");
     about.addCredit(i18n("Emanuele Bigiarini"), i18n("D-Bus and Konqueror plugin"), "pulmro@gmail.com");
     about.addCredit(i18n("Alex Infantes"), i18n("Improvements on Image preview plugin"),
                     "alexandro82@gmail.com");

microblogs/laconica/laconicamicroblog.cpp
@@ -61,7 +61,6 @@ LaconicaMicroBlog::LaconicaMicroBlog(QObject *parent, const QVariantList &)
     setServiceName("GNU social");
     mTimelineInfos["ReTweets"]->name = i18nc("Timeline name", "Repeated");
     mTimelineInfos["ReTweets"]->description = i18nc("Timeline description", "Your posts that were repeated by others");
-//     setServiceHomepageUrl("http://twitter.com/");
 }
 
 LaconicaMicroBlog::~LaconicaMicroBlog()

microblogs/ocs/ocsmicroblog.cpp
@@ -303,7 +303,7 @@ QString OCSMicroblog::profileUrl(Choqok::Account *account, const QString &userna
 {
     OCSAccount *acc = qobject_cast<OCSAccount *>(account);
     if (acc->providerUrl().host().contains("opendesktop.org")) {
-        return QString("http://opendesktop.org/usermanager/search.php?username=%1").arg(username);
+        return QString("https://opendesktop.org/usermanager/search.php?username=%1").arg(username);
     }
     return QString();
 }

plugins/shorteners/is_gd/is_gd.cpp
@@ -48,7 +48,7 @@ QString Is_gd::shorten(const QString &url)
 {
     Is_gd_Settings::self()->load();
 
-    QUrl reqUrl("http://is.gd/create.php");
+    QUrl reqUrl("https://is.gd/create.php");
     reqUrl.addQueryItem("format", "json");
     reqUrl.addQueryItem("url", QUrl(url).url());
     if (Is_gd_Settings::logstats()) {

plugins/uploaders/flickr/flickr.cpp
@@ -54,7 +54,7 @@ Flickr::~Flickr()
 
 void Flickr::upload(const QUrl &localUrl, const QByteArray &medium, const QByteArray &mediumType)
 {
-    QUrl url("http://api.flickr.com/services/upload/");
+    QUrl url("https://api.flickr.com/services/upload/");
     FlickrSettings::self()->load();
     QString token = Choqok::PasswordManager::self()->readPassword(QString("flickr_%1")
                     .arg(FlickrSettings::username()));
@@ -154,9 +154,9 @@ void Flickr::slotUpload(KJob *job)
 
                     QString remUrl;
                     if (FlickrSettings::shorturl()) {
-                        remUrl = "http://flic.kr/p/" + base58encode(photoId.toULongLong());
+                        remUrl = "https://flic.kr/p/" + base58encode(photoId.toULongLong());
                     } else {
-                        remUrl = "http://flickr.com/photos/" + FlickrSettings::nsid() + '/' + photoId;
+                        remUrl = "https://flickr.com/photos/" + FlickrSettings::nsid() + '/' + photoId;
                     }
 
                     Q_EMIT mediumUploaded(localUrl, remUrl);

plugins/uploaders/flickr/flickrconfig.cpp
@@ -126,7 +126,7 @@ void FlickrConfig::emitChanged()
 void FlickrConfig::getFrob()
 {
     m_frob.clear();
-    QUrl url("http://flickr.com/services/rest/");
+    QUrl url("https://flickr.com/services/rest/");
     url.addQueryItem("method", "flickr.auth.getFrob");
     url.addQueryItem("api_key", apiKey.toUtf8());
     url.addQueryItem("api_sig",  createSign("methodflickr.auth.getFrob"));
@@ -185,7 +185,7 @@ void FlickrConfig::getFrob()
 void FlickrConfig::getToken()
 {
     m_token.clear();
-    QUrl url("http://flickr.com/services/rest/");
+    QUrl url("https://flickr.com/services/rest/");
     url.addQueryItem("method", "flickr.auth.getToken");
     url.addQueryItem("api_key", apiKey.toUtf8());
     url.addQueryItem("frob", m_frob.toUtf8());
@@ -286,7 +286,7 @@ void FlickrConfig::slotAuthButton_clicked()
 {
     getFrob();
     if (!m_frob.isEmpty()) {
-        QUrl oUrl("http://flickr.com/services/auth/?");
+        QUrl oUrl("https://flickr.com/services/auth/?");
         oUrl.setPath(oUrl.path() + "api_key=" + apiKey +
                      "&perms=write&frob=" + m_frob +
                      "&api_sig=" + createSign("frob" + m_frob.toUtf8() + "permswrite"));

plugins/uploaders/imageshack/imageshack.cpp
@@ -53,7 +53,7 @@ void ImageShack::upload(const QUrl &localUrl, const QByteArray &medium, const QB
         Q_EMIT uploadingFailed(localUrl, i18n("Just supporting image uploading"));
         return;
     }
-    QUrl url("http://www.imageshack.us/upload_api.php");
+    QUrl url("https://www.imageshack.us/upload_api.php");
     QMap<QString, QByteArray> formdata;
     formdata["key"] = apiKey.toLatin1();
     formdata["rembar"] = "1";

commit 49fec9f6808b7db12cc10465621b5782d07b0e6a

Fix Twitpic previews
commit 98aecec8a067d204e85ecdd18d744a9a20c5128f broke it because it checks
for HTTPS urls only.

plugins/imagepreview/imagepreview.cpp
@@ -35,7 +35,7 @@
 K_PLUGIN_FACTORY_WITH_JSON(ImagePreviewFactory, "choqok_imagepreview.json",
                            registerPlugin < ImagePreview > ();)
 
-const QRegExp ImagePreview::mTwitpicRegExp("(https://twitpic.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");
+const QRegExp ImagePreview::mTwitpicRegExp("(https?://twitpic.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");
 const QRegExp ImagePreview::mYFrogRegExp("(http://yfrog.[^\\s<>\"]+[^!,\\.\\s<>'\\\"\\]])");
 const QRegExp ImagePreview::mTweetphotoRegExp("(http://tweetphoto.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");
 const QRegExp ImagePreview::mPlixiRegExp("(http://plixi.com/[^\\s<>\"]+[^!,\\.\\s<>'\"\\]])");