KDE/choqok

commit 45c5f08d293762440ce67ef45e8c116882f6cc8d

Move configuration stuff away from KDELibs4Support

choqok/config/appearance/appearanceconfig.cpp
@@ -21,14 +21,13 @@ along with this program; if not, see http://www.gnu.org/licenses/
 
 */
 #include "appearanceconfig.h"
-#include "ui_appearanceconfig_base.h"
 
+#include <QWidget>
 #include <QVBoxLayout>
 
 #include <KAboutData>
 #include <KLocalizedString>
 #include <KPluginFactory>
-#include <KTabWidget>
 
 #include "appearancedebug.h"
 #include "choqokappearancesettings.h"
@@ -36,49 +35,15 @@ along with this program; if not, see http://www.gnu.org/licenses/
 K_PLUGIN_FACTORY_WITH_JSON(ChoqokAppearanceConfigFactory, "choqok_appearanceconfig.json",
                            registerPlugin<AppearanceConfig>();)
 
-class AppearanceConfig::Private
-{
-public:
-    Private()
-        : mAppearanceTabCtl(0L)
-    {}
-
-    KTabWidget *mAppearanceTabCtl;
-
-    Ui::AppearanceConfig_Base mPrefsBase;
-};
-
 AppearanceConfig::AppearanceConfig(QWidget *parent, const QVariantList &args)
     : KCModule(KAboutData::pluginData("kcm_choqok_appearanceconfig"), parent, args)
-    , d(new Private())
 {
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    // since the tab widget is already within a layout with margins in the KSettings::Dialog
-    // it needs no margins of its own.
-    layout->setContentsMargins(0, 0, 0, 0);
-    d->mAppearanceTabCtl = new KTabWidget(this);
-    d->mAppearanceTabCtl->setTabBarHidden(true);
-    d->mAppearanceTabCtl->setDocumentMode(true);
-    d->mAppearanceTabCtl->setObjectName("mAppearanceTabCtl");
-    layout->addWidget(d->mAppearanceTabCtl);
-
-//     KConfigGroup config(KGlobal::config(), "ChatWindowSettings");
-
-    // "Contact List" TAB =======================================================
-    QWidget *appearsWidget = new QWidget(d->mAppearanceTabCtl);
-    d->mPrefsBase.setupUi(appearsWidget);
-    addConfig(Choqok::AppearanceSettings::self(), appearsWidget);
-
-    d->mAppearanceTabCtl->addTab(appearsWidget, i18n("Appearance"));
-
-    // ==========================================================================
-
+    setupUi(this);
     load();
 }
 
 AppearanceConfig::~AppearanceConfig()
 {
-    delete d;
 }
 
 void AppearanceConfig::save()

choqok/config/appearance/appearanceconfig.h
@@ -24,12 +24,14 @@ along with this program; if not, see http://www.gnu.org/licenses/
 #ifndef APPEARANCECONFIG_H
 #define APPEARANCECONFIG_H
 
+#include "ui_appearanceconfig_base.h"
+
 #include <KCModule>
 
 /**
  * @author Mehrdad Momeny \<mehrdad.momeny@gmail.com\>
  */
-class AppearanceConfig : public KCModule
+class AppearanceConfig : public KCModule, public Ui_AppearanceConfig_Base
 {
     Q_OBJECT
 
@@ -37,11 +39,8 @@ public:
     AppearanceConfig(QWidget *parent, const QVariantList &args);
     ~AppearanceConfig();
 
+public Q_SLOTS:
     virtual void save();
     virtual void load();
-
-private:
-    class Private;
-    Private *const d;
 };
 #endif

choqok/config/CMakeLists.txt
@@ -1,5 +1,4 @@
-
-add_subdirectory(behavior)
 add_subdirectory(accounts)
 add_subdirectory(appearance)
+add_subdirectory(behavior)
 add_subdirectory(plugins)

choqok/config/accounts/CMakeLists.txt
@@ -1,10 +1,11 @@
 set(kcm_choqok_accountsconfig_PART_SRCS
-accountsdebug.cpp
-accountswidget.cpp
-addaccountdialog.cpp
-editaccountdialog.cpp )
+    accountsdebug.cpp
+    accountswidget.cpp
+    addaccountdialog.cpp
+    editaccountdialog.cpp
+)
 
-ki18n_wrap_ui(kcm_choqok_accountsconfig_PART_SRCS accountswidget_base.ui )
+ki18n_wrap_ui(kcm_choqok_accountsconfig_PART_SRCS accountswidget_base.ui)
 
 add_library(kcm_choqok_accountsconfig MODULE ${kcm_choqok_accountsconfig_PART_SRCS})
 
@@ -12,13 +13,16 @@ kcoreaddons_desktop_to_json(kcm_choqok_accountsconfig choqok_accountsconfig.desk
 
 target_link_libraries(kcm_choqok_accountsconfig
 PUBLIC
+    Qt5::Core
     Qt5::Widgets
     KF5::CoreAddons
+    KF5::ConfigWidgets
+    KF5::I18n
     KF5::Service
     KF5::WidgetsAddons
     KF5::KDELibs4Support
     choqok
 )
 
-install(TARGETS kcm_choqok_accountsconfig  DESTINATION ${PLUGIN_INSTALL_DIR})
-install( FILES choqok_accountsconfig.desktop  DESTINATION ${SERVICES_INSTALL_DIR})
+install(TARGETS kcm_choqok_accountsconfig DESTINATION ${PLUGIN_INSTALL_DIR})
+install(FILES choqok_accountsconfig.desktop DESTINATION ${SERVICES_INSTALL_DIR})

choqok/config/accounts/accountswidget.cpp
@@ -24,9 +24,9 @@
 
 #include <QAction>
 #include <QCheckBox>
+#include <QMenu>
 
 #include <KAboutData>
-#include <KMenu>
 #include <KMessageBox>
 #include <KPluginFactory>
 #include <KPluginInfo>
@@ -240,9 +240,9 @@ void AccountsWidget::save()
     }
 }
 
-KMenu *AccountsWidget::createAddAccountMenu()
+QMenu *AccountsWidget::createAddAccountMenu()
 {
-    mBlogMenu = new KMenu(i18n("Select Micro-Blogging Service"), this);
+    mBlogMenu = new QMenu(i18n("Select Micro-Blogging Service"), this);
     const QList<KPluginInfo> list = Choqok::PluginManager::self()->availablePlugins("MicroBlogs");
     Q_FOREACH (const KPluginInfo &info, list) {
         QAction *act = new QAction(mBlogMenu);

choqok/config/accounts/accountswidget.h
@@ -27,7 +27,7 @@
 
 #include <KCModule>
 
-class KMenu;
+class QMenu;
 namespace Choqok
 {
 class Account;
@@ -65,8 +65,8 @@ private:
     QList<QTableWidgetItem *> takeRow(int row);
     void setRow(int row, const QList<QTableWidgetItem *> &rowItems);
     void addAccountToTable(Choqok::Account *account);
-    KMenu *createAddAccountMenu();
-    KMenu *mBlogMenu;
+    QMenu *createAddAccountMenu();
+    QMenu *mBlogMenu;
 };
 
 #endif

choqok/config/accounts/accountswidget_base.ui
@@ -11,7 +11,16 @@
    </rect>
   </property>
   <layout class="QGridLayout" name="gridLayout">
-   <property name="margin">
+   <property name="leftMargin">
+    <number>1</number>
+   </property>
+   <property name="topMargin">
+    <number>1</number>
+   </property>
+   <property name="rightMargin">
+    <number>1</number>
+   </property>
+   <property name="bottomMargin">
     <number>1</number>
    </property>
    <property name="spacing">
@@ -23,7 +32,7 @@
       <number>6</number>
      </property>
      <item>
-      <widget class="KPushButton" name="btnAdd">
+      <widget class="QPushButton" name="btnAdd">
        <property name="toolTip">
         <string>Add a new account</string>
        </property>
@@ -33,7 +42,7 @@
       </widget>
      </item>
      <item>
-      <widget class="KPushButton" name="btnEdit">
+      <widget class="QPushButton" name="btnEdit">
        <property name="enabled">
         <bool>false</bool>
        </property>
@@ -46,7 +55,7 @@
       </widget>
      </item>
      <item>
-      <widget class="KPushButton" name="btnRemove">
+      <widget class="QPushButton" name="btnRemove">
        <property name="enabled">
         <bool>false</bool>
        </property>
@@ -72,7 +81,7 @@
       </spacer>
      </item>
      <item>
-      <widget class="KPushButton" name="btnUp">
+      <widget class="QPushButton" name="btnUp">
        <property name="enabled">
         <bool>false</bool>
        </property>
@@ -82,7 +91,7 @@
       </widget>
      </item>
      <item>
-      <widget class="KPushButton" name="btnDown">
+      <widget class="QPushButton" name="btnDown">
        <property name="enabled">
         <bool>false</bool>
        </property>
@@ -155,13 +164,6 @@
    </item>
   </layout>
  </widget>
- <customwidgets>
-  <customwidget>
-   <class>KPushButton</class>
-   <extends>QPushButton</extends>
-   <header>kpushbutton.h</header>
-  </customwidget>
- </customwidgets>
  <tabstops>
   <tabstop>accountsTable</tabstop>
   <tabstop>btnAdd</tabstop>

choqok/config/appearance/CMakeLists.txt
@@ -4,7 +4,7 @@ set(kcm_choqok_appearanceconfig_PART_SRCS
 )
 
 ki18n_wrap_ui(kcm_choqok_appearanceconfig_PART_SRCS
-appearanceconfig_base.ui )
+    appearanceconfig_base.ui)
 
 add_library(kcm_choqok_appearanceconfig MODULE ${kcm_choqok_appearanceconfig_PART_SRCS})
 
@@ -14,10 +14,10 @@ target_link_libraries(kcm_choqok_appearanceconfig
 PUBLIC
     Qt5::Widgets
     KF5::CoreAddons
-    KF5::KDELibs4Support
+    KF5::ConfigWidgets
+    KF5::I18n
     choqok
 )
 
-install(TARGETS kcm_choqok_appearanceconfig  DESTINATION ${PLUGIN_INSTALL_DIR})
-install( FILES choqok_appearanceconfig.desktop  DESTINATION ${SERVICES_INSTALL_DIR})
-
+install(TARGETS kcm_choqok_appearanceconfig DESTINATION ${PLUGIN_INSTALL_DIR})
+install(FILES choqok_appearanceconfig.desktop DESTINATION ${SERVICES_INSTALL_DIR})

commit c3886128193bbf3f215325d41caa7d40287fc95b

Restore Appeareance preferences load/save
Broken by commit 45c5f08d293762440ce67ef45e8c116882f6cc8d

choqok/config/appearance/appearanceconfig.cpp
@@ -25,6 +25,7 @@ along with this program; if not, see http://www.gnu.org/licenses/
 #include <QWidget>
 
 #include <KAboutData>
+#include <KConfigDialog>
 #include <KPluginFactory>
 
 #include "appearancedebug.h"
@@ -37,23 +38,14 @@ AppearanceConfig::AppearanceConfig(QWidget *parent, const QVariantList &args)
     : KCModule(KAboutData::pluginData("kcm_choqok_appearanceconfig"), parent, args)
 {
     setupUi(this);
+
+    addConfig(Choqok::AppearanceSettings::self(), this);
+
     load();
 }
 
 AppearanceConfig::~AppearanceConfig()
 {
-}
-
-void AppearanceConfig::save()
-{
-    KCModule::save();
-    qCDebug(CHOQOK) << "called";
-}
-
-void AppearanceConfig::load()
-{
-    KCModule::load();
-    qCDebug(CHOQOK) << "called";
 }
 
 #include "appearanceconfig.moc"

choqok/config/appearance/appearanceconfig.h
@@ -38,9 +38,5 @@ class AppearanceConfig : public KCModule, public Ui_AppearanceConfig_Base
 public:
     AppearanceConfig(QWidget *parent, const QVariantList &args);
     ~AppearanceConfig();
-
-public Q_SLOTS:
-    virtual void save();
-    virtual void load();
 };
 #endif