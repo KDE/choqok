KDE/choqok

commit 7af408c57e63e3a6ef9cf96fdedd5dcfa3eee8f7

Fix postId retrieval in TwitterAPI
Also, drop non existant homepage attribute retrieval.

helperlibs/twitterapihelper/twitterapimicroblog.cpp
@@ -215,7 +215,6 @@ QList< Choqok::Post* > TwitterApiMicroBlog::loadTimeline( Choqok::Account *accou
             st->author.isProtected = grp.readEntry("isProtected", false);
             st->isPrivate = grp.readEntry( "isPrivate" , false );
             st->author.location = grp.readEntry("authorLocation", QString());
-            st->author.homePageUrl = grp.readEntry("authorUrl", QString());
             st->link = postUrl( account, st->author.userName, st->postId);
             st->isRead = grp.readEntry("isRead", true);
             st->repeatedFromUsername = grp.readEntry("repeatedFrom", QString());
@@ -269,7 +268,6 @@ void TwitterApiMicroBlog::saveTimeline(Choqok::Account *account,
             grp.writeEntry( "isPrivate" , post->isPrivate );
             grp.writeEntry( "authorLocation" , post->author.location );
             grp.writeEntry( "isProtected" , post->author.isProtected );
-            grp.writeEntry( "authorUrl" , post->author.homePageUrl );
             grp.writeEntry( "isRead" , post->isRead );
             grp.writeEntry( "repeatedFrom", post->repeatedFromUsername);
             grp.writeEntry( "repeatedPostId", post->repeatedPostId );
@@ -1294,8 +1292,8 @@ Choqok::Post* TwitterApiMicroBlog::readPost(Choqok::Account* theAccount,
     post->content = var["text"].toString();
     post->creationDateTime = dateFromString(var["created_at"].toString());
     post->isFavorited = var["favorited"].toBool();
-    post->postId = var["id"].toString();
-    post->replyToPostId = var["in_reply_to_status_id"].toString();
+    post->postId = var["id_str"].toString();
+    post->replyToPostId = var["in_reply_to_status_id_str"].toString();
     post->replyToUserId = var["in_reply_to_user_id"].toString();
     post->replyToUserName = var["in_reply_to_screen_name"].toString();
     post->source = var["source"].toString();
@@ -1305,7 +1303,6 @@ Choqok::Post* TwitterApiMicroBlog::readPost(Choqok::Account* theAccount,
     post->author.userId = userMap["id"].toString();
     post->author.userName = userMap["screen_name"].toString();
     post->author.profileImageUrl = userMap["profile_image_url"].toString();
-    post->author.homePageUrl = userMap["statusnet_profile_url"].toString();
     QVariantMap entities = var["entities"].toMap();
     QVariantMap mediaMap;
     QVariantList media = entities["media"].toList();

libchoqok/mediamanager.cpp
@@ -69,7 +69,6 @@ MediaManager::~MediaManager()
 {
     delete d;
     mSelf = 0L;
-    qCDebug(CHOQOK);
 }
 
 MediaManager * MediaManager::mSelf = 0L;

commit fd5dfb25694b01e520e200289e72b77c6b9ccc8a

[TwitterApi] Always use id_str as dev.twitter suggest
[GNU Social] Fix in_reply_to_status_id value which we broke with
commit 7af408c57e63e3a6ef9cf96fdedd5dcfa3eee8f7.

[GNU Social] Read external_url from JSON post reply.
[GNU Social] Read in_reply_to_profile_url from JSON post reply.

helperlibs/twitterapihelper/twitterapimicroblog.cpp
@@ -1298,8 +1298,8 @@ Choqok::Post *TwitterApiMicroBlog::readPost(Choqok::Account *theAccount,
     post->content = var["text"].toString();
     post->creationDateTime = dateFromString(var["created_at"].toString());
     post->isFavorited = var["favorited"].toBool();
-    post->postId = var["id_str"].toString();
-    post->replyToPostId = var["in_reply_to_status_id_str"].toString();
+    post->postId = var["id"].toString();
+    post->replyToPostId = var["in_reply_to_status_id"].toString();
     post->replyToUserId = var["in_reply_to_user_id"].toString();
     post->replyToUserName = var["in_reply_to_screen_name"].toString();
     post->source = var["source"].toString();
@@ -1423,21 +1423,11 @@ Choqok::Post *TwitterApiMicroBlog::readDirectMessage(Choqok::Account *theAccount
 
 Choqok::User *TwitterApiMicroBlog::readUserInfo(const QByteArray &buffer)
 {
-    //qCCritical(CHOQOK)<<"TwitterApiMicroBlog::readUserInfoFromJson: NOT IMPLEMENTED YET!";
-    Choqok::User *user = new Choqok::User;
+    Choqok::User *user;
     const QJsonDocument json = QJsonDocument::fromJson(buffer);
     if (!json.isNull()) {
-        const QVariantMap map = json.toVariant().toMap();
-        // iterate over the list
-        user->description = map["description"].toString();
-        user->followersCount = map["followers_count"].toUInt();
-        user->homePageUrl = map["url"].toString();
-        user->isProtected = map["protected"].toBool();
-        user->location = map["location"].toString();
-        user->profileImageUrl = map["profile_image_url"].toString();
-        user->realName = map["name"].toString();
-        user->userId = map["id"].toString();
-        user->userName = map["screen_name"].toString();
+        Choqok::User u(readUser(0, json.toVariant().toMap()));
+        user = &u;
     } else {
         QString err = i18n("Retrieving the friends list failed. The data returned from the server is corrupted.");
         qCDebug(CHOQOK) << "JSON parse error: the buffer is: \n" << buffer;
@@ -1485,7 +1475,7 @@ Choqok::User TwitterApiMicroBlog::readUser(Choqok::Account *theAccount, const QV
     u.location = map["location"].toString();
     u.profileImageUrl = map["profile_image_url"].toString();
     u.realName = map["name"].toString();
-    u.userId = map["id"].toString();
+    u.userId = map["id_str"].toString();
     u.userName = map["screen_name"].toString();
     return u;
 }

microblogs/laconica/CMakeLists.txt
@@ -4,6 +4,7 @@ include_directories(
 )
 
 set(choqok_laconica_SRCS
+    gnusocialpost.cpp
     laconicaaccount.cpp
     laconicadebug.cpp
     laconicamicroblog.cpp

microblogs/laconica/gnusocialpost.cpp
@@ -1,0 +1,31 @@
+/*
+    This file is part of Choqok, the KDE micro-blogging client
+
+    Copyright (C) 2015  Andrea Scarpino <scarpino@kde.org>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy
+    defined in Section 14 of version 3 of the license.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see http://www.gnu.org/licenses/
+*/
+
+#include "gnusocialpost.h"
+
+GNUSocialPost::GNUSocialPost() : Post()
+{
+}
+
+GNUSocialPost::~GNUSocialPost()
+{
+}

microblogs/laconica/gnusocialpost.h
@@ -1,0 +1,39 @@
+/*
+    This file is part of Choqok, the KDE micro-blogging client
+
+    Copyright (C)  Andrea Scarpino <scarpino@kde.org>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy
+    defined in Section 14 of version 3 of the license.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see http://www.gnu.org/licenses/
+*/
+
+#ifndef GNUSOCIALPOST_H
+#define GNUSOCIALPOST_H
+
+#include <QStringList>
+
+#include "choqoktypes.h"
+
+class GNUSocialPost : public Choqok::Post
+{
+public:
+    explicit GNUSocialPost();
+    virtual ~GNUSocialPost();
+
+    QString replyToProfileUrl;
+};
+
+#endif //GNUSOCIALPOST_H

microblogs/laconica/laconicamicroblog.cpp
@@ -44,6 +44,7 @@ along with this program; if not, see http://www.gnu.org/licenses/
 #include "twitterapipostwidget.h"
 #include "twitterapitimelinewidget.h"
 
+#include "gnusocialpost.h"
 #include "laconicaaccount.h"
 #include "laconicacomposerwidget.h"
 #include "laconicadebug.h"
@@ -115,15 +116,23 @@ Choqok::UI::ComposerWidget *LaconicaMicroBlog::createComposerWidget(Choqok::Acco
 Choqok::Post *LaconicaMicroBlog::readPost(Choqok::Account *account, const QVariantMap &var, Choqok::Post *post)
 {
     if (!post) {
-        qCCritical(CHOQOK) << "LaconicaMicroBlog::readPost: post is NULL!";
+        qCCritical(CHOQOK) << "post is NULL!";
         return 0;
     }
 
     post = TwitterApiMicroBlog::readPost(account, var, post);
 
-    post->postId = var["id"].toString();
+    post->link = var["external_url"].toString();
 
-    return post;
+    GNUSocialPost *p = dynamic_cast< GNUSocialPost * >(post);
+    if (!p) {
+        qCCritical(CHOQOK) << "post is not a GNUSocialPost!";
+        return 0;
+    }
+
+    p->replyToProfileUrl = var["in_reply_to_profileurl"].toString();
+
+    return p;
 }
 
 QString LaconicaMicroBlog::profileUrl(Choqok::Account *account, const QString &username) const

microblogs/twitter/twittermicroblog.cpp
@@ -387,4 +387,23 @@ Twitter::List TwitterMicroBlog::readListFromJsonMap(Choqok::Account *theAccount,
     return l;
 }
 
+Choqok::Post *TwitterMicroBlog::readPost(Choqok::Account *account, const QVariantMap &var, Choqok::Post *post)
+{
+    if (!post) {
+        qCCritical(CHOQOK) << "TwitterMicroBlog::readPost: post is NULL!";
+        return 0;
+    }
+
+    post = TwitterApiMicroBlog::readPost(account, var, post);
+
+    post->postId = var["id_str"].toString();
+    post->replyToPostId = var["in_reply_to_status_id_str"].toString();
+    post->replyToUserId = var["in_reply_to_user_id_str"].toString();
+
+    QVariantMap userMap = var["user"].toMap();
+    post->author.userId = userMap["id_str"].toString();
+
+    return post;
+}
+
 #include "twittermicroblog.moc"

microblogs/twitter/twittermicroblog.h
@@ -81,6 +81,8 @@ protected Q_SLOTS:
 protected:
     QList<Twitter::List> readUserListsFromJson(Choqok::Account *theAccount, QByteArray buffer);
     Twitter::List readListFromJsonMap(Choqok::Account *theAccount, QVariantMap map);
+    using TwitterApiMicroBlog::readPost;
+    virtual Choqok::Post *readPost(Choqok::Account *account, const QVariantMap &var, Choqok::Post *post);
     QMap<KJob *, QString> mFetchUsersListMap;
     virtual void setTimelineInfos();
 private: