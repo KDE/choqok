43514f129a2cf1d8918a6c9b4be7c3b64115b275
Andrea Scarpino <scarpino@kde.org>
2015-08-14 20:33:46 +0200

    Fix wrong mail address

--- microblogs/twitter/twitterdmessagedialog.h
+++ microblogs/twitter/twitterdmessagedialog.h
@@ -1,7 +1,7 @@
 /*
     This file is part of Choqok, the KDE micro-blogging client
 
-    Copyright (C) 2015 Andrea Scarpino <andrea@archlinux.org>
+    Copyright (C) 2015 Andrea Scarpino <scarpino@kde.org>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
