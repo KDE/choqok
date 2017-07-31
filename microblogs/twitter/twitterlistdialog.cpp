KDE/choqok

commit 44c549a66de3f3e5efe57ba150c860fee8660f09

Replace Iterators with Q_FOREACH

microblogs/twitter/twitterlistdialog.cpp
@@ -120,20 +120,18 @@ void TwitterListDialog::loadUserLists()
 void TwitterListDialog::slotLoadUserlists(Choqok::Account *theAccount, QString username,
         QList<Twitter::List> list)
 {
-    if (theAccount == account && username == ui.username->text() && !list.isEmpty()) {
+    if (theAccount == account && QString::compare(username, ui.username->text()) && !list.isEmpty()) {
         listWidget->clear();
-        QList<Twitter::List>::const_iterator it = list.constBegin();
-        QList<Twitter::List>::const_iterator endIt = list.constEnd();
-        for (; it != endIt; ++it) {
+        Q_FOREACH (const Twitter::List &l, list) {
             QListWidgetItem *item = new QListWidgetItem(listWidget);
             QString iText;
-            if (it->description.isEmpty()) {
-                iText = it->fullname;
+            if (l.description.isEmpty()) {
+                iText = l.fullname;
             } else {
-                iText = QStringLiteral("%1 [%2]").arg(it->fullname).arg(it->description);
+                iText = QStringLiteral("%1 [%2]").arg(l.fullname).arg(l.description);
             }
             item->setText(iText);
-            item->setData(32, it->slug);
+            item->setData(32, l.slug);
             listWidget->addItem(item);
         }
         connect(listWidget, SIGNAL(itemClicked(QListWidgetItem*)),

helperlibs/twitterapihelper/twitterapimicroblog.cpp
@@ -245,9 +245,8 @@ void TwitterApiMicroBlog::saveTimeline(Choqok::Account *account,
             postsBackup.deleteGroup(group);
         }
 
-        QList< Choqok::UI::PostWidget *>::const_iterator it, endIt = timeline.constEnd();
-        for (it = timeline.constBegin(); it != endIt; ++it) {
-            const Choqok::Post *post = ((*it)->currentPost());
+        Q_FOREACH (Choqok::UI::PostWidget *wd, timeline) {
+            const Choqok::Post *post = (wd->currentPost());
             KConfigGroup grp(&postsBackup, post->creationDateTime.toString());
             grp.writeEntry("creationDateTime", post->creationDateTime);
             grp.writeEntry("postId", post->postId);
@@ -1347,21 +1346,17 @@ QList< Choqok::Post * > TwitterApiMicroBlog::readTimeline(Choqok::Account *theAc
     QList<Choqok::Post *> postList;
     const QJsonDocument json = QJsonDocument::fromJson(buffer);
     if (!json.isNull()) {
-        QVariantList list = json.toVariant().toList();
-        QVariantList::const_iterator it = list.constBegin();
-        QVariantList::const_iterator endIt = list.constEnd();
-        for (; it != endIt; ++it) {
-            postList.prepend(readPost(theAccount, it->toMap(), new Choqok::Post));
+        Q_FOREACH (const QVariant &list, json.toVariant().toList()) {
+            postList.prepend(readPost(theAccount, list.toMap(), new Choqok::Post));
         }
     } else {
-        QString err = checkForError(buffer);
+        const QString err = checkForError(buffer);
         if (err.isEmpty()) {
             qCCritical(CHOQOK) << "JSON parsing failed.\nBuffer was: \n" << buffer;
             Q_EMIT error(theAccount, ParsingError, i18n("Could not parse the data that has been received from the server."));
         } else {
             Q_EMIT error(theAccount, ServerError, err);
         }
-        return postList;
     }
     return postList;
 }
@@ -1440,21 +1435,17 @@ QList< Choqok::Post * > TwitterApiMicroBlog::readDirectMessages(Choqok::Account 
     QList<Choqok::Post *> postList;
     const QJsonDocument json = QJsonDocument::fromJson(buffer);
     if (!json.isNull()) {
-        QVariantList list = json.toVariant().toList();
-        QVariantList::const_iterator it = list.constBegin();
-        QVariantList::const_iterator endIt = list.constEnd();
-        for (; it != endIt; ++it) {
-            postList.prepend(readDirectMessage(theAccount, it->toMap()));
+        Q_FOREACH (const QVariant &list, json.toVariant().toList()) {
+            postList.prepend(readDirectMessage(theAccount, list.toMap()));
         }
     } else {
-        QString err = checkForError(buffer);
+        const QString err = checkForError(buffer);
         if (err.isEmpty()) {
             qCCritical(CHOQOK) << "JSON parsing failed.\nBuffer was: \n" << buffer;
             Q_EMIT error(theAccount, ParsingError, i18n("Could not parse the data that has been received from the server."));
         } else {
             Q_EMIT error(theAccount, ServerError, err);
         }
-        return postList;
     }
     return postList;
 }

libchoqok/mediamanager.cpp
@@ -222,14 +222,12 @@ QByteArray MediaManager::createMultipartFormData(const QMap< QString, QByteArray
         }
     }
 
-    QMap< QString, QByteArray >::const_iterator it = formdata.constBegin();
-    QMap< QString, QByteArray >::const_iterator endIt = formdata.constEnd();
-    for (; it != endIt; ++it) {
+    Q_FOREACH (const QString &key, formdata.keys()) {
         data.append(newLine);
         data.append(header);
-        data.append(formHeader.arg(it.key()).toLatin1());
+        data.append(formHeader.arg(key).toLatin1());
         data.append(newLine);
-        data.append(newLine + it.value());
+        data.append(newLine + formdata.value(key));
     }
     data.append(newLine);
     data.append(footer);

libchoqok/ui/choqoktextedit.cpp
@@ -290,18 +290,17 @@ void TextEdit::setupSpeller()
         d->curLang = s.defaultLanguage();
     }
     qCDebug(CHOQOK) << "Current LANG:" << d->curLang;
-    QMap<QString, QString> list = s.availableDictionaries();
-    QMap<QString, QString>::const_iterator it = list.constBegin(), endIt = list.constEnd();
-    for (; it != endIt; ++it) {
-        QAction *act = new QAction(it.key(), d->langActions);
-        act->setData(it.value());
+    Q_FOREACH (const QString &dict, s.availableDictionaries().keys()) {
+        const QString value = s.availableDictionaries().value(dict);
+        QAction *act = new QAction(dict, d->langActions);
+        act->setData(value);
         act->setCheckable(true);
-        if (d->curLang == it.value()) {
+        if (d->curLang == value) {
             act->setChecked(true);
         }
         connect(act, SIGNAL(triggered(bool)), SLOT(slotChangeSpellerLanguage()));
         d->langActions->addAction(act);
-        d->langActionMap.insert(it.value(), act);
+        d->langActionMap.insert(value, act);
     }
 }
 

libchoqok/ui/timelinewidget.cpp
@@ -97,9 +97,8 @@ void TimelineWidget::loadTimeline()
     if (!BehaviorSettings::markAllAsReadOnExit()) {
         addNewPosts(list);
     } else {
-        QList<Post *>::const_iterator it, endIt = list.constEnd();
-        for (it = list.constBegin(); it != endIt; ++it) {
-            PostWidget *pw = d->currentAccount->microblog()->createPostWidget(d->currentAccount, *it, this);
+        Q_FOREACH (Choqok::Post *p, list) {
+            PostWidget *pw = d->currentAccount->microblog()->createPostWidget(d->currentAccount, p, this);
             if (pw) {
                 pw->setRead();
                 addPostWidgetToUi(pw);
@@ -218,13 +217,12 @@ void TimelineWidget::addPlaceholderMessage(const QString &msg)
 void TimelineWidget::addNewPosts(QList< Choqok::Post * > &postList)
 {
     qCDebug(CHOQOK) << d->currentAccount->alias() << d->timelineName << postList.count();
-    QList<Post *>::const_iterator it, endIt = postList.constEnd();
     int unread = 0;
-    for (it = postList.constBegin(); it != endIt; ++it) {
-        if (d->posts.keys().contains((*it)->postId)) {
+    Q_FOREACH (Choqok::Post *p, postList) {
+        if (d->posts.keys().contains(p->postId)) {
             continue;
         }
-        PostWidget *pw = d->currentAccount->microblog()->createPostWidget(d->currentAccount, *it, this);
+        PostWidget *pw = d->currentAccount->microblog()->createPostWidget(d->currentAccount, p, this);
         if (pw) {
             addPostWidgetToUi(pw);
             if (!pw->isRead()) {

microblogs/ocs/ocsmicroblog.cpp
@@ -69,9 +69,8 @@ void OCSMicroblog::saveTimeline(Choqok::Account *account, const QString &timelin
         postsBackup.deleteGroup(group);
     }
 
-    QList< Choqok::UI::PostWidget *>::const_iterator it, endIt = timeline.constEnd();
-    for (it = timeline.constBegin(); it != endIt; ++it) {
-        const Choqok::Post *post = (*it)->currentPost();
+    Q_FOREACH (Choqok::UI::PostWidget *wd, timeline) {
+        const Choqok::Post *post = wd->currentPost();
         KConfigGroup grp(&postsBackup, post->creationDateTime.toString());
         grp.writeEntry("creationDateTime", post->creationDateTime);
         grp.writeEntry("postId", post->postId);
@@ -283,12 +282,10 @@ void OCSMicroblog::slotDefaultProvidersLoaded()
     mIsOperational = true;
     Q_EMIT initialized();
 
-    QMultiMap<Choqok::Account *, Task>::const_iterator it = scheduledTasks.constBegin();
-    QMultiMap<Choqok::Account *, Task>::const_iterator endIt = scheduledTasks.constEnd();
-    for (; it != endIt; ++it) {
-        switch (it.value()) {
+    Q_FOREACH (Choqok::Account *acc, scheduledTasks.keys()) {
+        switch (scheduledTasks.value(acc)) {
         case Update:
-            updateTimelines(it.key());
+            updateTimelines(acc);
             break;
         default:
             break;

microblogs/pumpio/pumpiomicroblog.cpp
@@ -461,9 +461,8 @@ void PumpIOMicroBlog::saveTimeline(Choqok::Account *account, const QString &time
         postsBackup.deleteGroup(group);
     }
 
-    QList< Choqok::UI::PostWidget *>::const_iterator it, endIt = timeline.constEnd();
-    for (it = timeline.constBegin(); it != endIt; ++it) {
-        PumpIOPost *post = dynamic_cast<PumpIOPost * >((*it)->currentPost());
+    Q_FOREACH (Choqok::UI::PostWidget *wd, timeline) {
+        PumpIOPost *post = dynamic_cast<PumpIOPost * >(wd->currentPost());
         KConfigGroup grp(&postsBackup, post->creationDateTime.toString());
         grp.writeEntry("creationDateTime", post->creationDateTime);
         grp.writeEntry("postId", post->postId);

microblogs/twitter/twittermicroblog.cpp
@@ -360,11 +360,8 @@ QList< Twitter::List > TwitterMicroBlog::readUserListsFromJson(Choqok::Account *
     if (!json.isNull()) {
         const QVariantMap map = json.toVariant().toMap();
         if (map.contains(QLatin1String("lists"))) {
-            QVariantList list = map[QLatin1String("lists")].toList();
-            QVariantList::const_iterator it = list.constBegin();
-            QVariantList::const_iterator endIt = list.constEnd();
-            for (; it != endIt; ++it) {
-                twitterList.append(readListFromJsonMap(theAccount, it->toMap()));
+            Q_FOREACH (const QVariant &list, map[QLatin1String("lists")].toList()) {
+                twitterList.append(readListFromJsonMap(theAccount, list.toMap()));
             }
         }
     }

commit 74816c3bb49f1f3029642d5159256b2eaf5957d6

Fix twitter lists fetching
Regression introduced by 44c549a66

BUG: 382392

microblogs/twitter/twitterlistdialog.cpp
@@ -120,7 +120,7 @@ void TwitterListDialog::loadUserLists()
 void TwitterListDialog::slotLoadUserlists(Choqok::Account *theAccount, QString username,
         QList<Twitter::List> list)
 {
-    if (theAccount == account && QString::compare(username, ui.username->text()) && !list.isEmpty()) {
+    if (theAccount == account && QString::compare(username, ui.username->text()) == 0 && !list.isEmpty()) {
         listWidget->clear();
         for (const Twitter::List &l: list) {
             QListWidgetItem *item = new QListWidgetItem(listWidget);